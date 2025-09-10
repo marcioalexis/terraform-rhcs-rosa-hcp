d5a01c04da7f5f2f4adc7b9a444b2d61c4fe27fb
marcioalexis <94806863+marcioalexis@users.noreply.github.com>
2025-09-09 21:00:57 -0300

    Update variables.tf

--- modules/rosa-cluster-hcp/variables.tf
+++ modules/rosa-cluster-hcp/variables.tf
@@ -3,22 +3,10 @@ variable "cluster_name" {
   description = "Name of the cluster. After the creation of the resource, it is not possible to update the attribute value."
 }
 
-variable "enable_default_ingress" {
-  description = "Controla se o recurso default_ingress deve ser criado"
-  type        = bool
-  default     = false
-}
-
 variable "aws_region" {
   type        = string
   default     = null
   description = "The full name of the AWS region used for the ROSA cluster installation, for example 'us-east-1'. If no information is provided, the data will be retrieved from the currently connected account."
-}
-
-variable "cluster_id" {
-  description = "ID do cluster para ser usado no recurso default_ingress"
-  type        = string
-  default     = ""
 }
 
 variable "path" {
